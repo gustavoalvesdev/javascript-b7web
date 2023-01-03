1802c7323db4f410cf728cb179390939efb1823c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-01-03 18:48:14 -0300

    Exercício de funções #6 (mais fácil)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,8 @@
-const m2 = 3000;
-
-function calcularImovel(metragem, quartos) {
-    let preco = 0;
-
-    if (quartos === 1) {
-        preco = metragem * m2;
-    } else if (quartos === 2) {
-        preco = metragem * m2 * 1.2;
-    } else if (quartos === 3) {
-        preco = metragem * m2 * 1.5;
-    }
-
-    return preco;
+function validaUsuario(usuario, senha) {
+    return usuario === 'pedro' && senha == '1234';
 }
 
-let metragem = 123;
-let quartos = 2;
-let preco = calcularImovel(metragem, quartos);
-console.log(`A casa custa R$ ${preco}`);
+let usuario = 'pedro';
+let senha = 1234;
+
+console.log('Usuário logou? ' + (validaUsuario(usuario, senha) ? 'Sim' : 'Nâo'));
