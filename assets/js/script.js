daf8bb82490e3de2dbd48b374dc04bfead06ee86
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-01-27 21:58:46 -0300

    Introdução a Array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-const sumSquare = (a, b) => {
-    const square = x => x * x
+// const fruits = ['Maçã', 'Banana', 'Limão', 'Cereja', 'Morango', 'Abacaxi'];
 
-    return square(a) + square(b)
-}
+// let tenths = [10, 20, 30, 40, 50, 60];
 
-console.log(sumSquare(5, 5));
+// fruits.map(fruit => { console.log(fruit) });
+
+// console.log(tenths[3]);
+
+// function squares(numbers) {
+//     return numbers.map(number => number * number);
+// }
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let numbersDouble = squares(numbers);
+
+// numbersDouble.map(number => {console.log(number)});
+
+let arrayOfArrays = [[1, 2, 3], ['Pedro', 'Zé', 'Maria']];
+
+console.log(arrayOfArrays[1][1]);
