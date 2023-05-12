7cacc6640c2545f18277197ed10d4e9790c3a2f3
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-05-11 23:54:47 -0300

    Exercício de array #7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,18 @@
-let ingredients = ['Farinha', 'Ovos', 'Manteiga', 'Óleo', 'Água'];
+// 1. No array abaixo, qual o número que pega a Ferrari?
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1;
+console.log('1. ' + carros[x]);
 
-// ingredients[5] = 'Creme';
+// 2. Troque a Ferrari por Audi
+carros[1] = 'Audi';
+console.log('2. Lista com Audi:');
+console.log(carros);
 
-// ingredients[18] = 'Biscoito';
+// 3. Adicione o Volvo à lista
+carros.push('Volvo');
+console.log('3. Lista com Volvo:');
+console.log(carros);
 
-// ingredients.push('Creme');
-
-// ingredients.pop();
-
-// console.log(`Quantidade de ingredientes: ${ingredients.length}`);
-
-// console.log('Listando ingredientes: ');
-
-
-// ingredients.map(ingredient => {
-//     console.log(ingredient);
-// });
-
-let cars = [];
-
-cars.push('Mustang');
-cars.push('Camaro');
-cars.push('Ferrari');
-cars.push('Porsche');
-
-cars.map(car => { console.log(car) })
-
-cars.shift();
-
-cars.map(car => { console.log(car) })
+// 4. Exiba quantos itens têm no array
+console.log('4. items no array:');
+console.log(carros.length);
