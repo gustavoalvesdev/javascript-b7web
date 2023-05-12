99bbb446944b2c8d8789c5d7a7e8a05ad5f0f6d6
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-05-12 00:07:10 -0300

    O que são objetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
-// 1. No array abaixo, qual o número que pega a Ferrari?
-let carros = ['BMW', 'Ferrari', 'Mercedes'];
-let x = 1;
-console.log('1. ' + carros[x]);
+let personagem = {
+    nome: 'Bonieky',
+    idade: 90,
+    pais: 'Brasil',
+    olhos: ['preto', 'azul'],
+    caracteristicas: {
+        forca: 20,
+        magia: 5,
+        stamina: 15
+    },
+    
+}
 
-// 2. Troque a Ferrari por Audi
-carros[1] = 'Audi';
-console.log('2. Lista com Audi:');
-console.log(carros);
-
-// 3. Adicione o Volvo à lista
-carros.push('Volvo');
-console.log('3. Lista com Volvo:');
-console.log(carros);
-
-// 4. Exiba quantos itens têm no array
-console.log('4. items no array:');
-console.log(carros.length);
+console.log(`${personagem.nome} tem ${personagem.idade} anos e mora no ${personagem.pais}`);
+console.log(`${personagem.caracteristicas.magia}`);
+console.log(personagem.olhos[1]);
