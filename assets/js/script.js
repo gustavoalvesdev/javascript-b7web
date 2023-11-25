dbb9164e1d48257e381b4eb7ef4ad3f642fb5a93
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-11-25 18:11:08 -0300

    Acessando e alterando objetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,10 @@
 let personagem = {
     nome: 'Bonieky',
     idade: 90,
-    pais: 'Brasil',
-    olhos: ['preto', 'azul'],
-    caracteristicas: {
-        forca: 20,
-        magia: 5,
-        stamina: 15
-    },
-    
+    carros: [
+        {modelo: 'Fiat', cor: 'preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
 }
 
-console.log(`${personagem.nome} tem ${personagem.idade} anos e mora no ${personagem.pais}`);
-console.log(`${personagem.caracteristicas.magia}`);
-console.log(personagem.olhos[1]);
+console.log(personagem.carros[1].modelo);
