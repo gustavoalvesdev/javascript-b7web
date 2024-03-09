cd347e0ad6e8ee0cbe1ecf8dec0470d6027d06c9
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-03-08 23:37:06 -0300

    Dando loop em arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-for (let i = 0; i < 10; i+=2) {
-    console.log('Repetindo 10 vezes');
-}   
+// let cores = ['preto', 'branco', 'azul', 'vermelho'];
+// cores.push('verde');
+
+let cores = [
+    {nome: 'preto', qt: 10},
+    {nome: 'azul', qt: 5},
+    {nome: 'vermelho', qt: 15}
+];
+
+// for (i = 0; i < cores.length; i++) {
+//     console.log(cores[i]);
+// }
+
+// for (let i in cores) {
+//     console.log(cores[i]);
+// }
+
+// for (let cor of cores) {
+//     console.log(cor);
+// }
+for (let cor of cores) {
+    console.log(`Nome: ${cor.nome.toUpperCase()} - ${cor.qt}`);
+}
