f1129d0054d5d11860d83e45eb9c54d2d7904fbd
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-03 18:02:01 -0300

    O loop while

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,7 @@
-// let cores = ['preto', 'branco', 'azul', 'vermelho'];
-// cores.push('verde');
-
-let cores = [
-    {nome: 'preto', qt: 10},
-    {nome: 'azul', qt: 5},
-    {nome: 'vermelho', qt: 15}
-];
-
-// for (i = 0; i < cores.length; i++) {
-//     console.log(cores[i]);
-// }
-
-// for (let i in cores) {
-//     console.log(cores[i]);
-// }
-
-// for (let cor of cores) {
-//     console.log(cor);
-// }
-for (let cor of cores) {
-    console.log(`Nome: ${cor.nome.toUpperCase()} - ${cor.qt}`);
+while (true) {
+    console.log(`Meu nome é pedro`)
+    let nome = prompt('Qual seu nome?')
+    if (nome.toLowerCase() != 'pedro') break
 }
+
+console.log('Saiu do loop')
