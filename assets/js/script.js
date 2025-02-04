7ec2f4c3e4b79ae1678718ebac9b02eeb8af095f
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-04 17:14:47 -0300

    Classes: Instância

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 class Person {
+
+    profession = 'Programmer'
+
     constructor(name, age) {
         this.name = name
         this.age = age
     }
 }
+
+let p1 = new Person('Joao', 23)
+let p2 = new Person('Maria', 25)
+let p3 = new Person('Pedro', '27')
+
+console.log(p1.name)
+console.log(p2.name)
+console.log(p1.profession)
+console.log(p2.profession)
+p1.age = 45
+console.log(p1.age)
