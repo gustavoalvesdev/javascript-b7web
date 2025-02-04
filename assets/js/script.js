e984057f54dc1fbe5bb681d8c8a8576666241e68
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-04 16:46:12 -0300

    Ordenação de array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-let fruits = ['Maçã', 'Uva', 'Laranja', 'Banana']
+let cars = [
+    {brand: 'Fiat', year: 2022},
+    {brand: 'Bmw', year: 2018},
+    {brand: 'Ferrari', year: 2020}
+]
 
+cars.sort((a, b) => a.year - b.year)
 
-
-console.log(fruits.join(' / '))
-
-fruits[fruits.length - 1] = 'Pêra'
-
-fruits.pop()
-fruits.push('Limão')
-
-console.log(fruits)
+console.log(cars)
