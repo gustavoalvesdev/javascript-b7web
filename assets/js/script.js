e546a2dcb10857a3febd7b191ce0cde8b29cd510
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-04 17:02:36 -0300

    POO no Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Programação Orientada a Objetos (POO)
+ * OOP (Object Oriented Programming)
+ * Programação Funcional 
+ * Functional Programming
+ * Programação Procedural
+ * - CLASSES
+ * - FUNÇÕES / OBJETOS
+ */
