cf82caf2510b4c680bc3bd4b7480fa5e01f5020c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-04 17:23:16 -0300

    Classes :Action

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,31 @@
 class Person {
 
     profession = 'Programmer'
+    steps = 0
 
     constructor(name, age) {
         this.name = name
         this.age = age
     }
+
+    takeAStep() {
+        this.steps++
+    }
+
+    setAge(newAge) {
+        if (typeof newAge == 'number') this.age = newAge
+    }
 }
 
 let p1 = new Person('Joao', 23)
 let p2 = new Person('Maria', 25)
-let p3 = new Person('Pedro', '27')
+let p3 = new Person('Pedro', 27)
 
-console.log(p1.name)
-console.log(p2.name)
-console.log(p1.profession)
-console.log(p2.profession)
-p1.age = 45
-console.log(p1.age)
+p1.takeAStep()
+p1.takeAStep()
+
+console.log(`Passos de ${p1.name}: ${p1.steps}`)
+
+p2.setAge(66)
+
+console.log(`${p2.name} agora tem ${p2.age} anos`)
