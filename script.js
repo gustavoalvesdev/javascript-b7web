0eba29c68ebc66eb81dc34ad5e47a370509b6c22
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-04-12 23:21:02 -0300

    Projeto> Luta - Fazer o log

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let char = new Knight('Bonieky')
+let log = new Log(document.querySelector('.log'))
+
+let char = new Sorcerer('Bonieky')
 let monster = new LittleMonster()
 
 const stage = new Stage(
     char, 
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 )
 
 stage.start()
