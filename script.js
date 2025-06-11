35ecd9fca09c63fbbb3a0507e00e279b22f66de3
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-06-10 21:05:14 -0300

    projeto bateria

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let person = {
     lastName: 'Lacerda',
     age: 90,
     getFullName() {
-        return `${this.name} ${this.lastName} `
+        return `${this.name} ${this.lastName}`
     }
 }
 
