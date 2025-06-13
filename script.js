8abd82b5aa5c5bf5c8317a165a7b16e48cefb691
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-06-12 23:01:40 -0300

    Projeto Funcional: Criando Personagens

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const defaultUser = {
-    name: '',
-    email: '',
-    level: 1
-}
+const char = createKnight('Bonieky')
+const monster = createLittleMonster()
 
-let user1 = {
-    ...defaultUser,
-    name: 'Bonieky',
-    email: 'suporte@b7web.com.br'
-}
-
-let adm1 = {
-    ...defaultUser,
-    name: 'Admin Boladão',
-    email: 'adm@adminboladao.com.br',
-    level: 2
-}
-
-console.log(adm1)
+console.log(monster.name)
