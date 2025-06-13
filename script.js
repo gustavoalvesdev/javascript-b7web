4252b7dba7e4a624adcb1401111e28234cba0e3e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-06-12 22:42:30 -0300

    Functional: Construtor

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ let person = {
     age: 90,
     getFullName() {
         return `${this.name} ${this.lastName}`
+    },
+    start() {
+        console.log('Deu start na pessoa')
     }
 }
+
+person.start()
 
 console.log(person.getFullName())
