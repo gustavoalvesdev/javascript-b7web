0b6c92948d005e2b4733984fc5b5d70f9195a2b0
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-10-11 21:04:17 -0300

    Projeto: Blog do JSONPlaceholder 1

---
+++ projeto-blog/assets/js/script.js
@@ -0,0 +1,19 @@
+async function readPosts() {
+    let postArea = document.querySelector('.posts')
+    postArea.innerHTML = 'Carregando...';
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await response.json()
+
+    if (json.length > 0) {
+        postArea.innerHTML = ''
+        for (let i in json) {
+            let postHTML = `<div><h1>${json[i].title}</h1>${json[i].body}<hr></div>`
+            postArea.innerHTML += postHTML
+        }
+    } else {
+        postArea.innerHTML = 'Nenhum post para exibir'
+    }
+}
+
+readPosts()
