b61d0d4012cc71656f7f09708a1c0fab473b26e5
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-11-17 18:10:01 -0300

    Métodos de String (2/3)

---
+++ 3-manipulacoes-arrow-ajax-etc/metodos-de-string-2-3/script.js
@@ -0,0 +1,5 @@
+let nome = 'JavaScript'
+
+let resultado = nome.substr(4, 6)
+
+console.log(resultado)
---
+++ 3-manipulacoes-arrow-ajax-etc/metodos-de-string-3-3/script.js
@@ -0,0 +1,5 @@
+let nome = '1,2,3,4,5,6,7,8,9,10'
+
+let resultado = nome.split(',')
+
+console.log(resultado)
