b1cc8bcebe7244244df60fbba738d781f70d6ae5
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-12-16 19:53:49 -0300

    Métodos de Arrays (3/4)

---
+++ 3-manipulacoes-arrow-ajax-etc/metodos-de-arrays-3-4/script.js
@@ -0,0 +1,10 @@
+let lista = [49, 4, 9, 16, 25];
+
+// let lista2 = lista.map(item => item * 2);
+// let lista2 = lista.filter(item => item < 20);
+// let lista2 = lista.every(item => item < 100);
+let lista2 = lista.some(item => item < 50);
+
+let res = lista2;
+
+console.log(res);
