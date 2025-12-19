125a46d6fedc3a9ec4bce81fc769c74f0baed67d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-12-19 20:29:30 -0300

    Datas (2/3)

---
+++ 3-manipulacoes-arrow-ajax-etc/datas-2-3/script.js
@@ -0,0 +1,16 @@
+let d = new Date();
+let novoValor = '';
+// let novoValor = d.getFullYear(); // ano
+// let novoValor = d.getMonth(); // mês - começa em 0
+// let novoValor = d.getDay(); // retorna o dia da semana - 0 domingo
+// let novoValor = d.getDate(); // dia atual
+// let novoValor = d.getHours();
+// let novoValor = d.getMinutes();
+// let novoValor = d.getSeconds();
+// novoValor = d.getMilliseconds();
+// novoValor = d.getTime(); // timestamp - milisegundos de 1 / 1 / 1970 até agora
+//novoValor = Date.now(); // timestamp sem precisar criar objeto  ES5
+
+
+
+console.log(novoValor);
