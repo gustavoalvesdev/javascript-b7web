31e13429f78d2e7e977b4f82a206cffaf8977436
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-12-19 20:12:20 -0300

    Datas (1/3)

---
+++ 3-manipulacoes-arrow-ajax-etc/datas-1-3/script.js
@@ -0,0 +1,10 @@
+//let d = new Date();
+//let d = new Date(1991, 8, 25, 12, 30, 10);
+// let d = new Date('1991-09-25 15:13:23');
+// let d = new Date(2019,8);
+let d = new Date(-1255484534343);
+
+// console.log(d.toDateString());
+console.log(d.toUTCString());
+// console.log(d.toString());
+// console.log(d);
