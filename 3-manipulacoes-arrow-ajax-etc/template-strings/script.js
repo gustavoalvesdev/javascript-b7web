bd8b98d51e45c9d38b18b3c2bd55edcf5151936e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-01-14 20:27:14 -0300

    Template Strings

---
+++ 3-manipulacoes-arrow-ajax-etc/template-strings/script.js
@@ -0,0 +1,7 @@
+let nome = 'Gustavo';
+let idade = 34;
+
+let frase = `Meu nome é ${nome} e eu tenho ${idade} anos. Ano que vem farei ${++idade} anos`;
+let soma = `Resultado da soma 10 + 12 = ${10 + 12}`;
+
+console.log(frase); 
