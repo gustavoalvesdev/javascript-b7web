d4bb68c207969b08379fac188b374a10fd64ba72
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-02-07 19:38:53 -0300

    Arrow Functions

---
+++ 3-manipulacoes-arrow-ajax-etc/arrow-functions/script.js
@@ -0,0 +1,15 @@
+// function somar(x, y) {
+//     return x + y;
+// }
+
+// let somar = function(x, y) {
+//     return x + y;
+// };
+
+let somar = (x, y) => x + y;
+
+let letrasNome = nome => nome.length;
+
+console.log(letrasNome('Gustavo'));
+
+console.log(somar(10, 5));
