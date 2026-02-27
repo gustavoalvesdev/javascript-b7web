5979003137b2ac820bb74d8aa2da2e39c62e1113
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-02-27 19:55:45 -0300

    Includes e Repeat

---
+++ 3-manipulacoes-arrow-ajax-etc/includes-e-repeat/script.js
@@ -0,0 +1,18 @@
+// let lista = ['ovo', 'café', 'arroz', 'feijão', 'macarrão']
+
+// console.log(lista.includes('ovo'))
+
+// let nome = 'Bonieky'
+
+// let temLetra = (nome, letra) => 
+// 		nome.includes(letra) ? 
+// 		`${nome} tem ${letra}` : 
+// 		`${nome} não tem ${letra}`
+
+// console.log(temLetra('Gustavo', 't'))
+
+// console.log('x'.repeat(20))
+
+const repetirLetra = (letra, vezes) => letra.repeat(vezes)
+
+console.log(repetirLetra('G', 9))
